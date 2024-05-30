8702b5d60daf4bc554c9e82114172c12c140c143
Vahnky <129200564+Vahnky@users.noreply.github.com>
2024-05-30 14:31:01 +0200

    Error.php case error ajax

---
+++ php/error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" href="style/style.css">
+
+
+
+<body>
+<div class="ok">
+<p class="pok"> Erreur! Veuillez réessayer votre requête ultérieurement.</p>
+</div>
+</body>
+
+
+<form action='home'><input type="submit" class="ddg" value="Retour à la page principale"></form>
